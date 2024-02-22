8158e7fe7648ea6459fc570a2099293ea08a5a25
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-02-22 12:54:29 +0530

    Create NAND_gt_tb.v

---
+++ VERILOG/DAY_1/NAND_gt_tb.v
@@ -0,0 +1,19 @@
+module nandg_tb;
+reg A, B;
+wire Y;
+nandg nandgate(.A(A),.B(B),.Y(Y));
+initial begin
+  $dumpfile("nandg.vcd");
+  $dumpvars(1,nandg_tb);  
+end
+initial begin
+  A=1'b0;B=1'b0;
+  #10 A=1'b0;B= 1'b1;
+  #10 A=1'b1;B= 1'b0;
+  #10 A=1'b1;B=1'b1;
+  #10
+  $finish;
+end
+always @(Y)
+  $monitor( "time=%0t \tINPUT VALUES: \t A=%b B=%b \t output value Y =%b",$time,A,B,Y);
+endmodule

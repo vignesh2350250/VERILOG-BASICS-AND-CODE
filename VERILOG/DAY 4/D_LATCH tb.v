40bf82e8b52478ca65aa1cca748331ea5d1a204a
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-02-26 10:06:57 +0530

    Create D_LATCH tb.v

---
+++ VERILOG/DAY 4/D_LATCH tb.v
@@ -0,0 +1,21 @@
+module d_latch_tb;
+  reg d,en,rstn;
+  reg [2:0] delay;  
+  reg [1:0] delay2;  
+  integer i;  
+  d_latch  dl0 ( .d (d),.en (en),.rstn (rstn),.q (q));
+  initial begin
+    d <= 0;  
+    en <= 0;  
+    rstn <= 0;  
+    #10 rstn <= 1;  
+    for (i = 0; i < 5; i=i+1) 
+      begin  
+         delay = $random;  
+         delay2 = $random;  
+         #(delay2) en <= ~en;  
+         #(delay) d <= i;  
+      end  
+    $monitor ("[%0t] en=%0b d=%0b q=%0b", $time, en, d, q);  
+   end  
+endmodule  

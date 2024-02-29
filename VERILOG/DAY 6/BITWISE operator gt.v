1760197066f7eddd961c60988d17562fe0bbe214
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-02-29 05:40:15 +0530

    Update BITWISE operator gt.v

--- VERILOG/DAY 6/BITWISE operator gt.v
+++ VERILOG/DAY 6/BITWISE operator gt.v
@@ -4,12 +4,16 @@ module opbw(input [4:0]a,
             output reg [4:0]y2,
             output reg [4:0]y3,
             output reg [4:0]y4,
-            output reg [4:0]y5);
+            output reg [4:0]y5,
+            output reg [4:0]y6,
+            output reg [4:0]y7);
   always@(*) begin
      y1=a & b;
      y2=a | b;
      y3=a ^ b;
      y4=~a;
      y5=~&a;
+     y6=~|a;
+     y7=~^b;
   end
 endmodule

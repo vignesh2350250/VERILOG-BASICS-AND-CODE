d55764e75bc794032f71caa0a44383e1653f81b0
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-02-29 05:40:47 +0530

    Update BITWISE operator tb.v

--- VERILOG/DAY 6/BITWISE operator tb.v
+++ VERILOG/DAY 6/BITWISE operator tb.v
@@ -1,7 +1,7 @@
 module opbw_tb;
   reg [4:0] a, b;
-  wire [4:0] y1, y2, y3, y4, y5;
-  opbw jj(a,b,y1,y2,y3,y4,y5);
+  wire [4:0] y1, y2, y3, y4, y5,y6,y7;
+  opbw jj(a,b,y1,y2,y3,y4,y5,y6,y7);
 
   initial begin
     $dumpfile("opbw.vcd");
@@ -9,7 +9,7 @@ module opbw_tb;
   end
 
   initial begin
-    $monitor("a=%b b=%b o1=%b o2=%b o3=%b o4=%b o5=%b", a, b, y1, y2, y3, y4, y5);
+    $monitor("a=%b b=%b y1=%b y2=%b y3=%b y4=%b y5=%b y6=%b y7=%b", a, b, y1, y2, y3, y4, y5,y6,y7);
     a = $random; b = $random; #5;
     $finish;
   end

78e6122516af79de43634dda349ce077407e7ff1
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-02-29 07:14:37 +0530

    Create CASE EQUALITY gt.v

---
+++ VERILOG/DAY 6/CASE EQUALITY gt.v
@@ -0,0 +1,14 @@
+module opeq(input [3:0]a,
+            input [3:0]b,
+            output reg [5:0]y1,
+            output reg [5:0]y2,
+            output reg [5:0]y3,
+            output reg [5:0]y4);
+  always@(*) begin
+     y1=a == b;
+     y2=a != b;
+     y3=a ===b;
+     y4=a !==b;
+     
+  end
+endmodule

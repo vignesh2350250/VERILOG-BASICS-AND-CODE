e6f872f93e3411cd5200814e77f9b30ac29bf2fb
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-03-04 20:10:48 +0530

    Create LEFT shift gt.v

---
+++ VERILOG/DAY 9/LEFT shift gt.v
@@ -0,0 +1,25 @@
+module shiftreg(
+  input clk,
+  input rst,
+  input din,
+  input [3:0]val,
+  output reg [3:0] dout 
+);
+
+  reg [3:0] shifted_dout; 
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      shifted_dout <= 4'b0000;
+    end
+    else begin
+      shifted_dout <= {val[3:1],din };
+    end
+  end 
+
+  always @* begin
+    $monitor("shifted value=%b",shifted_dout );
+    dout <= shifted_dout; 
+  end
+
+endmodule

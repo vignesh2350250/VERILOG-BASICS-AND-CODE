c244fa5da75158c05469dd58ae30126229f6eb1c
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-05-06 23:01:16 +0530

    Create test.sv

---
+++ SV VERIFICATIONS/FULL ADDER/test.sv
@@ -0,0 +1,26 @@
+`include "environment.sv"
+class addertest;
+  
+  environment env;
+  virtual operation vintf;
+  mailbox mail;
+  function new(virtual operation vintf,mailbox mail);
+    this.vintf=vintf;
+    this.mail = mail;
+  endfunction
+  
+  task memory;
+    mail =new();
+    env = new(vintf,mail);
+    
+  endtask
+  
+  task run;
+    
+    fork  
+      env.memory;
+      env.run;
+      
+    join
+  endtask  
+endclass

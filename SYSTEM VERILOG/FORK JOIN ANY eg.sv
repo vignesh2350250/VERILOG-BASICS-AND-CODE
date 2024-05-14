62c603e6103d5137de4497ecf65e25e2067c6642
vignesh2350250 <140400414+vignesh2350250@users.noreply.github.com>
2024-05-14 11:06:19 +0530

    Create FORK JOIN ANY eg.sv

---
+++ SYSTEM VERILOG/FORK JOIN ANY eg.sv
@@ -0,0 +1,17 @@
+module fj;
+  initial begin
+    $display($time,"-- fork join eg");
+    #10 $display($time,"--sequential after 10");
+    fork
+      $display($time,"--parallel start");
+      #50 $display($time,"--parallel after 50");
+      #10 $display($time,"--parallel after 10");
+      begin
+        #30 $display($time,"--sequential after 30");
+        #10 $display($time,"--sequential after 10");
+      end
+    join_any
+    $display($time,"--ouside fork join");
+    #80 $display($time,"--final after 80");
+  end
+endmodule
